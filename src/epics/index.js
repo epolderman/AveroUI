69bf2c04f763ece8be8fd9aaea8c6356136d486e
Erik Polderman <epolderman@averoinc.com>
2018-03-16 12:53:00 -0600

    Adding epics

---
+++ src/epics/index.js
@@ -0,0 +1,6 @@
+import { fetchTables } from './actions/index';
+import { FETCH_TABLES } from './actions/types';
+import { $action } from 'rxjs';
+
+// const fetchTablesEpic = action$ =>
+//     action$.ofType(FETCH_TABLES)
--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import reduxObservable from 'redux-observable'; //here
 import reducers from './reducers';
 import { Provider } from 'react-redux';
-
-
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-  <Provider store={store} ><App /></Provider>,
-  document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 registerServiceWorker();
