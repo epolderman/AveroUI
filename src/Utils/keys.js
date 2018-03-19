95925b834298ca9555ad598595968d7ed40ddb04
Erik Polderman <epolderman@averoinc.com>
2018-03-19 17:41:15 -0600

    git rm cached keys

--- src/Utils/keys.js
+++
@@ -1,4 +0,0 @@
-export const AUTH_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcxOWNmNGVjLWUxY2QtNDFhZC04NWU5LTU3ODBhMjE4MzZiNyIsIm5hbWUiOiJFcmlrIFBvbGRlcm1hbiJ9.CpEZ14y8_s6u9F5mhPrhBtruNlTIVmzVPDq9afbQbjE';
-
-export const ROOT_URL = 'https://check-api.herokuapp.com';
